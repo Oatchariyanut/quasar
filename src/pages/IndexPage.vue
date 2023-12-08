fcc229fb37ea883710bfe10b5b48644dbb7a0f2c
Oatchariyanut <Oatchariyanut.38802@gmail.com>
2023-12-08 11:09:48 +0700

    Oatcariyanut 奥查里亚纳帕蒂塔 6404101378

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -12,7 +12,7 @@
         label="你的名字 *"
         hint="名和姓"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || '请输入一些内容']"
+        :rules="[ val => val && val.length > 0 || '请输入 一些内容']"
       />
 
       <q-input
